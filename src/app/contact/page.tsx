b2f977f2c47c36624f19397a1a83d7777a7db0b6
rizkypratamatan <rizkypratama@preloode.com>
2025-10-28 05:57:06 +0700

    Fix metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,14 +8,14 @@ import React from "react";
 
 export const metadata: Metadata = {
     metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL ?? '/'),
-    title: process.env.NEXT_PUBLIC_WEBSITE_NAME,
+    title: `Contact - ${process.env.NEXT_PUBLIC_WEBSITE_NAME}`,
     description: `Contact of ${process.env.NEXT_PUBLIC_WEBSITE_NAME}. Fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.`,
     keywords: ['resume', 'fullstack developer', 'web development', 'frontend developer', 'backend developer', 'reactjs', 'javascript', 'python developer', 'rust developer', 'django developer', 'java developer', 'tailwindcss', 'web design', 'software engineer', 'web app development', 'mobile app development', 'scalable app'],
     alternates: {
         canonical: '/contact'
     },
     openGraph: {
-        title: process.env.NEXT_PUBLIC_WEBSITE_NAME,
+        title: `Contact - ${process.env.NEXT_PUBLIC_WEBSITE_NAME}`,
         description: `Contact of ${process.env.NEXT_PUBLIC_WEBSITE_NAME}. Fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.`,
         url: '/contact',
         siteName: process.env.NEXT_PUBLIC_WEBSITE_NAME,
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     },
     twitter: {
         card: 'summary_large_image',
-        title: process.env.NEXT_PUBLIC_WEBSITE_NAME,
+        title: `Contact - ${process.env.NEXT_PUBLIC_WEBSITE_NAME}`,
         description: 'Fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.',
         images: ['/images/avatars/rizky-pratama-profile-picture.png']
     }
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -10,14 +10,14 @@ import React from "react";
 
 export const metadata: Metadata = {
     metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL ?? '/'),
-    title: process.env.NEXT_PUBLIC_WEBSITE_NAME,
+    title: `Resume - ${process.env.NEXT_PUBLIC_WEBSITE_NAME}`,
     description: `Resume of ${process.env.NEXT_PUBLIC_WEBSITE_NAME}. Fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.`,
     keywords: ['resume', 'fullstack developer', 'web development', 'frontend developer', 'backend developer', 'reactjs', 'javascript', 'python developer', 'rust developer', 'django developer', 'java developer', 'tailwindcss', 'web design', 'software engineer', 'web app development', 'mobile app development', 'scalable app'],
     alternates: {
         canonical: '/resume'
     },
     openGraph: {
-        title: process.env.NEXT_PUBLIC_WEBSITE_NAME,
+        title: `Resume - ${process.env.NEXT_PUBLIC_WEBSITE_NAME}`,
         description: `Resume of ${process.env.NEXT_PUBLIC_WEBSITE_NAME}. Fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.`,
         url: '/resume',
         siteName: process.env.NEXT_PUBLIC_WEBSITE_NAME,
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     },
     twitter: {
         card: 'summary_large_image',
-        title: process.env.NEXT_PUBLIC_WEBSITE_NAME,
+        title: `Resume - ${process.env.NEXT_PUBLIC_WEBSITE_NAME}`,
         description: 'Fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.',
         images: ['/images/avatars/rizky-pratama-profile-picture.png']
     }
