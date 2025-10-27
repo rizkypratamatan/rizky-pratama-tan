c7c93f2ce56847d35370cf69dd785a1044e4cb08
rizkypratamatan <rizkypratama@preloode.com>
2025-10-28 05:51:00 +0700

    Fix metadata

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -11,14 +11,14 @@ import React from "react";
 export const metadata: Metadata = {
     metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL ?? '/'),
     title: process.env.NEXT_PUBLIC_WEBSITE_NAME,
-    description: `Resume of ${process.env.NEXT_PUBLIC_WEBSITE_NAME} fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.`,
+    description: `Resume of ${process.env.NEXT_PUBLIC_WEBSITE_NAME}. Fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.`,
     keywords: ['resume', 'fullstack developer', 'web development', 'frontend developer', 'backend developer', 'reactjs', 'javascript', 'python developer', 'rust developer', 'django developer', 'java developer', 'tailwindcss', 'web design', 'software engineer', 'web app development', 'mobile app development', 'scalable app'],
     alternates: {
         canonical: '/resume'
     },
     openGraph: {
         title: process.env.NEXT_PUBLIC_WEBSITE_NAME,
-        description: `Resume of ${process.env.NEXT_PUBLIC_WEBSITE_NAME} fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.`,
+        description: `Resume of ${process.env.NEXT_PUBLIC_WEBSITE_NAME}. Fullstack software engineer specializing in scalable web applications with 13 years of experience across different programming language and proven track record in Rust, Python, React, Kotlin, C#, and PHP.`,
         url: '/resume',
         siteName: process.env.NEXT_PUBLIC_WEBSITE_NAME,
         images: [
