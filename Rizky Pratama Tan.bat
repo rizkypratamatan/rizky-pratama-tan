3e163e081481ba3ba2ce3d8984d3084dc2200b8f
rizkypratamatan <rizkypratama@preloode.com>
2026-01-06 20:55:15 +0700

    Add production setup

--- Rizky Pratama Tan.bat
+++ Rizky Pratama Tan.bat
@@ -1,4 +1,3 @@
 @echo off
 title Rizky Pratama Tan
-call pm2 list
-cmd /k pm2 start cmd --name "rizky-pratama-tan" -- /c "npm run start -- -p 5001"
+call pm2 resurrect
---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: "rizky-pratama-tan",
+            script: "cmd",
+            args: "/c npm start",
+            cwd: "C:/Server/Rizky Pratama/rizky-pratama-tan",
+            env: {
+                NODE_ENV: "production",
+                PORT: 5001
+            }
+        }
+    ]
+};
